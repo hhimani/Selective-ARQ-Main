48d7e513d95f38abd7fe8bcb4f4e92e2e48dac91
hhimani@ncsu.edu <hhimani@ncsu.edu>
2018-04-24 22:21:10 -0400

    updated client

--- client.py
+++ client.py
@@ -5,15 +5,17 @@ import struct
 import threading
 import os
 
+
+
 COMMAND_LINE_INPUT  = True
 
-N = 10
-Window_Lock = threading.Lock()
+N = 1
 Retransmit_time = 0.05
+N_Transit_Window = {}
+Window_Lock = threading.Lock()
+Packet_Transferring = True
+Last_frame_seq = -1
 Data_Packets = []
-Time_Stamp =[]
-Last_Ack = -1
-In_Transit = 0
 
 
 def Calculate_CheckSum(data):
@@ -33,6 +35,7 @@ def Create_packet(present_sequence,packet_data):
     header = struct.pack('!IHH',present_sequence, check_sum, 21845)
     packet = header + packet_data
     return packet
+
 
 def Make_data_packets(File_name,MSS):
     global Data_Packets
@@ -61,27 +64,59 @@ def Make_data_packets(File_name,MSS):
         print 'File dosenot exist in the given location. Please Check \n'
         sys.exit()
 
+def rdf_send(Server_Address, Client_Socket, File_name, N, MSS):
+    global N_Transit_Window
+    global Packet_Transferring
+    global Last_frame_seq
 
-def rdf_send(Server_Address, Client_Socket, N):
-    print 'rdf_send thread started'
-    global Data_Packets
-    global Last_Ack
-    global In_Transit
-    global Time_Stamp
+    sent = 0
+    while sent < len(Data_Packets):
+        if len(N_Transit_Window) < N:
+            Packet_Sender(Server_Address, Client_Socket, sent, Data_Packets[sent])
+            sent += 1  # send packet by adding header information
 
-    Time_Stamp = [None]*len(Data_Packets)
-    while (Last_Ack + 1) < len(Data_Packets):
+
+class Packet_Sender(threading.Thread):
+    def __init__(self, Server_Address, Client_Socket, Sequence_Number, packet_data):
+        threading.Thread.__init__(self)
+        self.Server_Address = Server_Address
+        self.Client_Socket = Client_Socket
+        self.Seq_num = Sequence_Number
+        self.data = packet_data
+        self.start()
+
+
+    def run(self):
+        global N_Transit_Window
+        global Window_Lock
+        global Packet_Transferring
+
+        packet = self.data
         Window_Lock.acquire()
-        if In_Transit < N and ((Last_Ack + In_Transit + 1) < len(Data_Packets)):            #Send More Packets
-            Client_Socket.sendto(Data_Packets[Last_Ack + In_Transit + 1], Server_Address)
-            Time_Stamp[Last_Ack + In_Transit + 1] = time.time()
-            In_Transit += 1
-        if In_Transit > 0:
-            if (time.time() - Time_Stamp[Last_Ack + 1]) > Retransmit_time:
-                print 'Time out, Sequence Number =' + str(Last_Ack+1)
-                In_Transit = 0
+        N_Transit_Window[self.Seq_num] = time.time()
+        try:
+            self.Client_Socket.sendto(packet, self.Server_Address)
+            if self.Seq_num == len(Data_Packets) -1:
+                Packet_Transferring = False
+        except:
+            print "Server closed its connection"
+            self.Client_Socket.close()
+            exit()
         Window_Lock.release()
 
+        try:
+            while self.Seq_num in N_Transit_Window:
+                Window_Lock.acquire()
+                if self.Seq_num in N_Transit_Window:
+                    if (time.time() - N_Transit_Window[self.Seq_num]) > Retransmit_time:
+                        print 'Time out, Sequence Number =' + str(self.Seq_num)
+                        N_Transit_Window[self.Seq_num] = time.time()
+                        self.Client_Socket.sendto(packet, self.Server_Address)
+                Window_Lock.release()
+        except:
+            print "Server closed its connection"
+            self.Client_Socket.close()
+            exit()
 
 
 def Split_Ack_Header(Ack_Data):
@@ -97,32 +132,27 @@ def Split_Ack_Header(Ack_Data):
 
 def Ack_Receiver(Client_Socket):
     print "Client Thread to Receive Acknowledgements Started \n"
-    global Last_Ack
-    global In_Transit
-    global Time_Stamp
-    global Data_Packets
+    global N_Transit_Window
+    global Window_Lock
+    global Packet_Transferring
 
     try:
-        while (Last_Ack + 1) < len(Data_Packets):
-            if In_Transit > 0:
+        while Packet_Transferring or len(N_Transit_Window) > 0 :
+            if len(N_Transit_Window) > 0:
                 Ack_Data, Server_Address = Client_Socket.recvfrom(2048)
                 Valid_frame, Sequence_Number = Split_Ack_Header(Ack_Data)
-                Window_Lock.acquire()
                 if Valid_frame:
-                    if Last_Ack+1 == Sequence_Number:
-                        Last_Ack += 1
-                        In_Transit -= 1
-                    else:
-                        In_Transit = 0
-                else:
-                    In_Transit = 0
-
-                Window_Lock.release()
-
+                    if Sequence_Number in N_Transit_Window:
+                        Window_Lock.acquire()
+                        del (N_Transit_Window[Sequence_Number])
+                        if Sequence_Number+1 == len(Data_Packets):
+                            print 'Last Frame Ack Recieved'
+                        Window_Lock.release()
     except:
         print "Server closed its connection"
         Client_Socket.close()
-        sys.exit()
+        exit()
+    print 'Acknowledement thread closing'
 
 
 def main():
@@ -134,37 +164,35 @@ def main():
         N = int(sys.argv[4])
         MSS = int(sys.argv[5])
     else:
-        server_host_name = '192.168.179.1'
-        server_port = 7735
+        server_host_name = '152.46.18.96'
+        server_port = 7736
         File_name = 's.txt'
         N = 10
         MSS = 500
 
+    Make_data_packets(File_name,MSS)
     Server_Address = (server_host_name, server_port)
-    #my_ip = socket.gethostbyname(socket.gethostname())
+    my_ip = socket.gethostbyname(socket.gethostname())
+    #my_ip = ''
     Client_Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     Client_Port = 1234
-    Client_Socket.bind(( '0.0.0.0' , Client_Port))
-    print 'running at port' + str(Client_Port) + 'and server ip addr is ' +str(Server_Address)
-    Make_data_packets(File_name,MSS)
+    Client_Socket.bind((my_ip, Client_Port))
+
 
     startTime = time.time()
 
     Ack_receiver_thread = threading.Thread(target=Ack_Receiver, args=(Client_Socket,))
-    rdf_send_thread = threading.Thread(target=rdf_send,args=(Server_Address, Client_Socket, N))
+    File_packet_sender_thread = threading.Thread(target=rdf_send,args=(Server_Address, Client_Socket, File_name, N, MSS))
 
     Ack_receiver_thread.start()
-    rdf_send_thread.start()
-
+    File_packet_sender_thread.start()
     Ack_receiver_thread.join()
-    rdf_send_thread.join()
+    File_packet_sender_thread.join()
     print 'Ending Program'
+
 
     endTime = time.time()
     print 'Total Time Taken:' + str(endTime - startTime)
-
-    if Client_Socket:
-        Client_Socket.close()
 
 if __name__ == '__main__':
     main()
